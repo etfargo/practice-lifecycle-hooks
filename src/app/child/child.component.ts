2020ed63ed50bfb609f5f863e452d3e6c997fb39
etfargo <etfargo@hotmail.com>
2021-04-09 10:32:03 -0500

    1 file modified, 1 added

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  selector: "app-child",
+  templateUrl: "./child.component.html",
+  styleUrls: ["./child.component.css"]
 })
-export class ChildComponent implements OnInit {
-
+export class ChildComponent implements OnInit, OnChanges {
   @Input() inputData: number;
 
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log("this is the constructor: " + this.inputData);
   }
 
+  ngOnInit() {
+    console.log("this is ngOnInit: " + this.inputData);
+  }
+
+  ngOnChanges() {
+    console.log("this is ngOnChanges: " + this.inputData);
+  }
 }
